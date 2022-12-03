a94003532a073661ae5f93e5a07f3d9c42c1381b
netouyo <119365487+netouyo@users.noreply.github.com>
2022-12-02 20:01:47 -0500

    a

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 console.log("ここはコンソール欄だよ、ここにたどり着いた君は相当な手練だね");
+$(function () {
+    function end_loader() {
+      $('.loader').fadeOut(800);
+    }
+    $(window).on('load', function () {
+      setTimeout(function () {
+        end_loader();
+      }, 3000)
+    })
+  })  
