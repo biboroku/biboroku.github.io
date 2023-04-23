2a34a6715f916842efc627b365bd908fc681afb5
netouyo <soutanosabu1@gmail.com>
2023-04-23 10:37:03 -0400

    a

--- articles/ms.js
+++ articles/ms.js
@@ -15,49 +15,6 @@ const levels = [
     const message = document.querySelector("h2");
     message.textContent = "";
   
-<<<<<<< HEAD
-    function startGame({ rows: r, cols: c, mines }) {
-      showMessage("");
-      rows = r;
-      cols = c;
-      cells = createCells(rows, cols, mines);
-      renderBoard(board, cells, rows, cols);
-      document.documentElement.style.setProperty("--board-width", width);
-    }
-  
-    function createCells(rows, cols, mines) {
-      const cells = new Array(rows * cols).fill(null).map((_, i) => ({
-        index: i,
-        isMine: false,
-        isRevealed: false,
-        isFlagged: false,
-      }));
-  
-      let placedMines = 0;
-      while (placedMines < mines) {
-        const index = Math.floor(Math.random() * cells.length);
-        if (!cells[index].isMine) {
-          cells[index].isMine = true;
-          placedMines++;
-        }
-      }
-      return cells;
-    }
-  
-    function renderBoard() {
-        board.innerHTML = "";
-        for (let index = 0; index < cells.length; index++) {
-          const el = document.createElement("div");
-          el.className = "cell";
-          el.addEventListener("contextmenu", (e) => {
-            e.preventDefault();
-            toggleFlag(index);
-          });
-          el.addEventListener("click", () => revealCell(index));
-          board.appendChild(el);
-        }
-      }      
-=======
     board.innerHTML = "";
     board.style.gridTemplateRows = `repeat(${rows}, 30px)`;
     board.style.gridTemplateColumns = `repeat(${cols}, 30px)`;
@@ -82,7 +39,6 @@ const levels = [
       } while (cells[index].isMine);
       cells[index].isMine = true;
     }
->>>>>>> parent of 6f1d076 (Update ms.js)
   
     function getCellIndex(row, col) {
       return row * cols + col;
@@ -93,69 +49,6 @@ const levels = [
     }
   
     function getNeighbors(index) {
-<<<<<<< HEAD
-      const { row, col } = getCellRowCol(index);
-      const neighbors = [];
-      for (let r = row - 1; r <= row + 1; r++) {
-        for (let c = col - 1; c <= col + 1; c++) {
-          if (r === row && c === col) continue;
-          if (r < 0 || r >= rows || c < 0 || c >= cols) continue;
-          neighbors.push(getCellIndex(r, c));
-        }
-      }
-      return neighbors;
-    }
-  
-    function countMinesAround(index) {
-      return getNeighbors(index).reduce((count, neighbor) => count + (cells[neighbor].isMine ? 1 : 0), 0);
-    }
-  
-    function revealCell(index, isChained) {
-        if (cells[index].isRevealed || cells[index].isFlagged) return;
-        cells[index].isRevealed = true;
-        const cell = board.children[index];
-        cell.classList.add("uncovered");
-      
-        if (cells[index].isMine) {
-          if (!isChained) {
-            cell.classList.add("mine");
-            gameOver();
-          }
-          return;
-        }
-      
-        const minesAround = countMinesAround(index);
-        if (minesAround > 0) {
-          cell.classList.add(`cell-${minesAround}`);
-          cell.textContent = minesAround;
-        } else {
-          getNeighbors(index).forEach((neighbor) => revealCell(neighbor, true));
-        }
-      
-        if (checkVictory()) {
-          showMessage("勝利！おめでとうございます！");
-        }
-      }                       
-  
-    function toggleFlag(index) {
-        if (cells[index].isRevealed) return;
-        cells[index].isFlagged = !cells[index].isFlagged;
-        const cell = board.children[index];
-        cell.classList.toggle("flag");
-        cell.textContent = cells[index].isFlagged ? "F" : "";
-      }      
-  
-    function checkVictory() {
-      return cells.every((cell) => (cell.isMine && cell.isFlagged) || (!cell.isMine && cell.isRevealed));
-    }
-  
-    function gameOver() {
-      cells.forEach((cell, index) => {
-        if (cell.isMine) {
-          board.children[index].classList.add("mine");
-          if (cell.isFlagged) {
-            board.children[index].classList.add("correct-flag");
-=======
         const { row, col } = getCellRowCol(index);
         const neighbors = [];
         for (let r = row - 1; r <= row + 1; r++) {
@@ -163,7 +56,6 @@ const levels = [
             if (r === row && c === col) continue;
             if (r < 0 || r >= rows || c < 0 || c >= cols) continue;
             neighbors.push(getCellIndex(r, c));
->>>>>>> parent of 6f1d076 (Update ms.js)
           }
         }
         return neighbors;
