b5dbc3c920ba6a9c78643e908bdda9862b8245d1
netouyo <soutanosabu1@gmail.com>
2023-04-23 11:30:05 -0400

    Revert "Revert "初手改善2""
    This reverts commit c517d340a758c8bf7902c85661368318f9f56ccb.

--- articles/ms.js
+++ articles/ms.js
@@ -21,122 +21,78 @@ document.querySelector("#start").addEventListener("click", () => {
   startGame(config.rows, config.cols, config.mines);
 });
 
+// ...（前のコードと同じ）
+
 function startGame(r, c, m) {
-  rows = r;
-  cols = c;
-  mines = m;
-
-  revealedCount = 0;
-  flaggedCount = 0;
-
-  resetTimer();
-
-  document.querySelector("h2").textContent = "";
-  document.querySelector("#board").innerHTML = "";
-  document.querySelector("#board").addEventListener("contextmenu", (e) => e.preventDefault());
-
-  for (let i = 0; i < rows * cols; i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.addEventListener("click", () => revealCell(i));
-    cell.addEventListener("contextmenu", (e) => {
-      e.preventDefault();
-      toggleFlag(i);
-    });
-    document.querySelector("#board").appendChild(cell);
+    rows = r;
+    cols = c;
+    mines = m;
+  
+    revealedCount = 0;
+    flaggedCount = 0;
+  
+    resetTimer();
+  
+    document.querySelector("h2").textContent = "";
+    document.querySelector("#board").innerHTML = "";
+    document.querySelector("#board").addEventListener("contextmenu", (e) => e.preventDefault());
+  
+    for (let i = 0; i < rows * cols; i++) {
+      const cell = document.createElement("div");
+      cell.classList.add("cell");
+      cell.addEventListener("click", () => revealCell(i));
+      cell.addEventListener("contextmenu", (e) => {
+        e.preventDefault();
+        toggleFlag(i);
+      });
+      document.querySelector("#board").appendChild(cell);
+    }
+  
+    // 盤面のレイアウトを調整する
+    renderBoard(rows, cols);
   }
-}
-
-function createBoard(rows, cols, mines, safeIndex) {
-  let minePositions = generateMinePositions(rows, cols, mines, safeIndex);
-  board = new Array(rows * cols).fill(0);
-
-  for (const pos of minePositions) {
-    board[pos] = MINE;
-  }
-
-  for (let i = 0; i < rows * cols; i++) {
-    if (board[i] === MINE) continue;
-    board[i] = countSurroundingMines(i, rows, cols, board);
-  }
-
-  renderBoard(rows, cols);
-}
-
-function generateMinePositions(rows, cols, mines, safeIndex) {
-  const positions = [];
-  const safePositions = getSurroundingIndexes(safeIndex, rows, cols).concat([safeIndex]);
-
-  while (positions.length < mines) {
-    const randIndex = Math.floor(Math.random() * (rows * cols));
-    if (!positions.includes(randIndex) && !safePositions.includes(randIndex)) {
-      positions.push(randIndex);
+  
+  // ...（前のコードと同じ）
+  
+  function revealCell(index) {
+    if (startTime === null) {
+      startTimer();
+      createBoard(rows, cols, mines, index);
+    }
+  
+    const cell = document.querySelector(`#board > .cell:nth-child(${index + 1})`);
+    if (cell.textContent === "F" || cell.classList.contains("revealed")) return;
+  
+    cell.classList.add("revealed");
+    revealedCount++;
+  
+    if (board[index] === MINE) {
+      cell.textContent = "●";
+      cell.style.color = "red";
+      cell.style.textDecoration = "none";
+      showMessage("ゲームオーバー");
+      revealAllMines();
+      return;
+    }
+  
+    cell.textContent = board[index] || "";
+    cell.style.color = ["", "blue", "green", "red", "purple", "maroon", "cyan", "black", "gray"][board[index]];
+  
+    // 背景色を変更
+    cell.style.backgroundColor = "#ddd";
+  
+    if (revealedCount === rows * cols - mines) {
+      showMessage("勝利！おめでとうございます！");
+      return;
+    }
+  
+    if (board[index] === 0) {
+      getSurroundingIndexes(index, rows, cols).forEach(revealCell);
     }
   }
-  return positions;
-}
-
-function countSurroundingMines(index, rows, cols, board) {
-  return getSurroundingIndexes(index, rows, cols)
-    .map((i) => board[i])
-    .filter((cell) => cell === MINE).length;
-}
-
-function getSurroundingIndexes(index, rows, cols) {
-  const directions = [
-    [-1, -1], [-1, 0], [-1, 1],
-    [0, -1], /* 0, 0 */ [0, 1],
-    [1, -1], [1, 0], [1, 1]
-  ];
-
-  const x = index % cols;
-  const y = Math.floor(index / cols);
-
-  return directions
-    .map(([dx, dy]) => [x + dx, y + dy])
-    .filter(([newX, newY]) => newX >= 0 && newX < cols && newY >= 0 && newY < rows)
-    .map(([newX, newY]) => newY * cols + newX);
-}
-
-function renderBoard(rows, cols) {
-  const boardElement = document.querySelector("#board");
-  boardElement.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-  boardElement.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-}
-
-function revealCell(index) {
-  if (startTime === null) {
-    startTimer();
-    createBoard(rows, cols, mines, index);
-  }
-
-  const cell = document.querySelector(`#board > .cell:nth-child(${index + 1})`);
-  if (cell.textContent === "F" || cell.classList.contains("revealed")) return;
-
-  cell.classList.add("revealed");
-  revealedCount++;
-
-  if (board[index] === MINE) {
-    cell.textContent = "●";
-    cell.style.color = "red";
-    cell.style.textDecoration = "none";
-    showMessage("ゲームオーバー");
-    revealAllMines();
-    return;
-  }
-
-  cell.textContent = board[index] || "";
-  cell.style.color = ["", "blue", "green", "red", "purple", "maroon", "cyan", "black", "gray"][board[index]];
-
-  if (revealedCount === rows * cols - mines) {
-    showMessage("勝利！おめでとうございます！");
-    return;
-  }
-
-  if (board[index] === 0) {
-    getSurroundingIndexes(index, rows, cols).forEach(revealCell);
-  }
-}
+  
+  // ...（残りのコードと同じ）
+  
 
 function toggleFlag(index) {
   const cell = document.querySelector(`#board > .cell:nth-child(${index + 1})`);
