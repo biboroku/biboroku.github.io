6f1d0762a8c1dc32318264ea0cdd92b05c23baca
netouyo <soutanosabu1@gmail.com>
2023-04-23 09:34:21 -0400

    Update ms.js

--- articles/ms.js
+++ articles/ms.js
@@ -1,43 +1,62 @@
-const levels = [
-    { rows: 9, cols: 9, mines: 10 },
-    { rows: 16, cols: 16, mines: 40 },
-    { rows: 30, cols: 16, mines: 99 },
-    { rows: 50, cols: 50, mines: 500 }
-  ];
+document.addEventListener("DOMContentLoaded", () => {
+    const difficulties = [
+      { rows: 9, cols: 9, mines: 10 },
+      { rows: 16, cols: 16, mines: 40 },
+      { rows: 30, cols: 16, mines: 99 },
+      { rows: 50, cols: 50, mines: 500 },
+    ];
   
-  document.querySelector("#start").addEventListener("click", () => {
-    const level = levels[document.querySelector("#difficulty").value];
-    startGame(level.rows, level.cols, level.mines);
-  });
+    const board = document.getElementById("board");
+    let cells;
+    let rows;
+    let cols;
   
-  function startGame(rows, cols, mines) {
-    const board = document.querySelector("#board");
-    const message = document.querySelector("h2");
-    message.textContent = "";
+    document.getElementById("start").addEventListener("click", () => {
+      const diff = document.getElementById("difficulty").value;
+      startGame(difficulties[diff]);
+    });
   
-    board.innerHTML = "";
-    board.style.gridTemplateRows = `repeat(${rows}, 30px)`;
-    board.style.gridTemplateColumns = `repeat(${cols}, 30px)`;
-  
-    const cells = [];
-    for (let i = 0; i < rows * cols; i++) {
-      const cell = document.createElement("div");
-      cell.classList.add("cell");
-      cell.addEventListener("click", () => revealCell(i));
-      cell.addEventListener("contextmenu", (e) => {
-        e.preventDefault();
-        toggleFlag(i);
-      });
-      board.appendChild(cell);
-      cells.push({ isMine: false, isRevealed: false, isFlagged: false });
+    function startGame({ rows: r, cols: c, mines }) {
+      showMessage("");
+      rows = r;
+      cols = c;
+      cells = createCells(rows, cols, mines);
+      renderBoard(board, cells, rows, cols);
     }
   
-    for (let i = 0; i < mines; i++) {
-      let index;
-      do {
-        index = Math.floor(Math.random() * cells.length);
-      } while (cells[index].isMine);
-      cells[index].isMine = true;
+    function createCells(rows, cols, mines) {
+      const cells = new Array(rows * cols).fill(null).map((_, i) => ({
+        index: i,
+        isMine: false,
+        isRevealed: false,
+        isFlagged: false,
+      }));
+  
+      let placedMines = 0;
+      while (placedMines < mines) {
+        const index = Math.floor(Math.random() * cells.length);
+        if (!cells[index].isMine) {
+          cells[index].isMine = true;
+          placedMines++;
+        }
+      }
+      return cells;
+    }
+  
+    function renderBoard(board, cells, rows, cols) {
+      board.innerHTML = "";
+      board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+      board.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+      cells.forEach((cell, index) => {
+        const el = document.createElement("div");
+        el.classList.add("cell");
+        el.addEventListener("click", () => revealCell(index));
+        el.addEventListener("contextmenu", (e) => {
+          e.preventDefault();
+          toggleFlag(index);
+        });
+        board.appendChild(el);
+      });
     }
   
     function getCellIndex(row, col) {
@@ -49,80 +68,88 @@ const levels = [
     }
   
     function getNeighbors(index) {
-        const { row, col } = getCellRowCol(index);
-        const neighbors = [];
-        for (let r = row - 1; r <= row + 1; r++) {
-          for (let c = col - 1; c <= col + 1; c++) {
-            if (r === row && c === col) continue;
-            if (r < 0 || r >= rows || c < 0 || c >= cols) continue;
-            neighbors.push(getCellIndex(r, c));
-          }
+      const { row, col } = getCellRowCol(index);
+      const neighbors = [];
+      for (let r = row - 1; r <= row + 1; r++) {
+        for (let c = col - 1; c <= col + 1; c++) {
+          if (r === row && c === col) continue;
+          if (r < 0 || r >= rows || c < 0 || c >= cols) continue;
+          neighbors.push(getCellIndex(r, c));
         }
-        return neighbors;
       }
-    
-      function countMinesAround(index) {
-        return getNeighbors(index).reduce((count, neighbor) => count + (cells[neighbor].isMine ? 1 : 0), 0);
-      }
-    
-      function revealCell(index) {
-        if (cells[index].isRevealed || cells[index].isFlagged) return;
-        cells[index].isRevealed = true;
-        const cell = board.children[index];
-        cell.classList.add("uncovered");
-    
-        if (cells[index].isMine) {
-          cell.classList.add("mine");
+      return neighbors;
+    }
+  
+    function countMinesAround(index) {
+      return getNeighbors(index).reduce((count, neighbor) => count + (cells[neighbor].isMine ? 1 : 0), 0);
+    }
+  
+    function revealCell(index, isChained) {
+      if (cells[index].isRevealed || cells[index].isFlagged) return;
+      cells[index].isRevealed = true;
+      const cell = board.children[index];
+      cell.classList.add("uncovered");
+  
+      if (cells[index].isMine) {
+        if (!isChained) {
+          cell.classList.add        ("mine");
           gameOver();
-          return;
         }
-    
-        const minesAround = countMinesAround(index);
-        if (minesAround > 0) {
-          cell.classList.add(`cell-${minesAround}`);
-          cell.textContent = minesAround;
-        } else {
-          getNeighbors(index).forEach(revealCell);
+        return;
+      }
+  
+      const minesAround = countMinesAround(index);
+      if (minesAround > 0) {
+        cell.classList.add(`cell-${minesAround}`);
+        cell.textContent = minesAround;
+        if (!isChained) {
+          checkForAutoReveal(index, minesAround);
         }
-    
-        if (checkVictory()) {
-          showMessage("勝利！おめでとうございます！");
-        }
+      } else {
+        getNeighbors(index).forEach((neighbor) => revealCell(neighbor, true));
       }
-    
-      function toggleFlag(index) {
-        if (cells[index].isRevealed) return;
-        cells[index].isFlagged = !cells[index].isFlagged;
-        const cell = board.children[index];
-        cell.classList.toggle("flagged");
-        cell.textContent = cells[index].isFlagged ? "F" : "";
-      }
-    
-      function checkVictory() {
-        return cells.every(
-          (cell, index) =>
-            (cell.isFlagged && cell.isMine) || (!cell.isMine && cell.isRevealed)
-        );
-      }
-    
-      function gameOver() {
-        cells.forEach((cell, index) => {
-          if (cell.isMine) {
-            const el = board.children[index];
-            el.textContent = "●";
-          } else if (cell.isFlagged) {
-            const el = board.children[index];
-            el.classList.remove("flagged");
-            el.classList.add("false-flag");
-            el.textContent = "F";
-          }
-        });
-        showMessage("ゲームオーバー");
-      }
-    
-      function showMessage(msg) {
-        const message = document.querySelector("h2");
-        message.textContent = msg;
+  
+      if (checkVictory()) {
+        showMessage("勝利！おめでとうございます！");
       }
     }
-      
+  
+    function toggleFlag(index) {
+      if (cells[index].isRevealed) return;
+      cells[index].isFlagged = !cells[index].isFlagged;
+      board.children[index].classList.toggle("flag");
+    }
+  
+    function checkVictory() {
+      return cells.every((cell) => (cell.isMine && cell.isFlagged) || (!cell.isMine && cell.isRevealed));
+    }
+  
+    function gameOver() {
+      cells.forEach((cell, index) => {
+        if (cell.isMine) {
+          board.children[index].classList.add("mine");
+          if (cell.isFlagged) {
+            board.children[index].classList.add("correct-flag");
+          }
+        } else if (cell.isFlagged) {
+          board.children[index].classList.add("incorrect-flag");
+        }
+      });
+      showMessage("ゲームオーバー");
+    }
+  
+    function showMessage(message) {
+      const messageEl = document.querySelector("h2");
+      messageEl.textContent = message;
+    }
+  
+    function checkForAutoReveal(index, minesAround) {
+      const neighbors = getNeighbors(index);
+      const flaggedNeighbors = neighbors.filter((neighbor) => cells[neighbor].isFlagged);
+      if (minesAround === flaggedNeighbors.length) {
+        const unflaggedNeighbors = neighbors.filter((neighbor) => !cells[neighbor].isFlagged);
+        unflaggedNeighbors.forEach((neighbor) => revealCell(neighbor, true));
+      }
+    }
+  });
+    
